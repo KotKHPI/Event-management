c2cbda240517b241b14e685188d83fa017d757f8
KotKHPI <prototype2428@gmail.com>
2023-08-25 10:37:41 +0300

    Set attendee controller

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('events.attendees', AttendeeController::class)
-    ->scoped(['attendee' => 'event']); //attendee always part of event
+//    ->scoped(['attendee' => 'event']); //attendee always part of event
+    ->scoped()->except('update');
