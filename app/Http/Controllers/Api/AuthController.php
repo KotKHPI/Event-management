0ec3489ff32d3ba4f4ff5458862589529ca328a0
KotKHPI <prototype2428@gmail.com>
2023-09-01 11:41:15 +0300

    Add logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -40,7 +40,11 @@ class AuthController extends Controller
         ]);
     }
 
-    public function logout() {
+    public function logout(Request $request) {
+        $request->user()->tokens()->delete();
 
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
