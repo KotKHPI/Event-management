2e8b2fb012fe976026d1ca6bae2e84b5e2a02019
KotKHPI <prototype2428@gmail.com>
2023-09-01 10:54:28 +0300

    Add auth api

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
