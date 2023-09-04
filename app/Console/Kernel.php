1d068b91d37c0f750d602b14a4fbd35512ce0840
KotKHPI <prototype2428@gmail.com>
2023-09-04 11:32:51 +0300

    Add e-mail notification

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('app:send-event-reminders')
-            ->everyMinute();
+            ->daily();
     }
 
     /**
